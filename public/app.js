42ba74f58adbb26ff4fbc77853d1c707c0b6167c
Luís Ramalho <lcasas@sga.pucminas.br>
2025-05-30 23:37:14 -0300

    alterações

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const APIurl = 'https://api.themoviedb.org/3/movie/popular?api_key=82e3160b362bd6e69585ed82fdf77260&language=pt-BR'
+const APIurl = 'https://api.themoviedb.org/3/tv/popular?api_key=82e3160b362bd6e69585ed82fdf77260&language=pt-BR'
 const carrossel = document.getElementById('carouselExampleCaptions');
 
 //função imprime
@@ -15,7 +15,7 @@ function imprimeCarrossel(filmes){
         <div class="carousel-item ${index === 0 ? 'active' : ''}">
         <a href="detalhes.html?id=${filme.id}"><img src="https://image.tmdb.org/t/p/w1280${filme.backdrop_path}" class="d-block w-100" alt="${filme.title}"></a>
         <div class="carousel-caption d-none d-md-block">
-        <h5>${filme.title}</h5>
+        <h5>${filme.name}</h5>
         <p>${limitarTexto(filme.overview, 150)}</p>
         </div>
         </div>
@@ -37,7 +37,6 @@ fetch(APIurl)
     console.error("erro:", error);
     document.getElementById('carrossel').textContent = 'erro ao carregar filmes';
 });
-
 
 //Função imprime séries novas
 function imprimeNovas(filmes){
@@ -73,24 +72,25 @@ fetch('https://api.themoviedb.org/3/tv/on_the_air?api_key=82e3160b362bd6e69585ed
 fetch('http://localhost:3000/aluno')
 .then((response) => response.json())
 .then(data => {
-    console.log(data);
     var aluno = data[0]; 
     const infoAluno = document.getElementById('infoAluno');
     const biografia = document.getElementById('miniBio');
     const social = document.getElementById('redesSocial');
 
     const infoSocial = `
-        <p><strong>Facebook:</strong> ${aluno.facebook}</p>
-        <p><strong>Twitter:</strong> ${aluno.twitter}</p>
-        <p><strong>Instagram:</strong> ${aluno.instagram}</p>
+        <p><strong>Facebook:</strong> <a href="facebook.com">${aluno.facebook}</a></p>
+        <p><strong>Twitter:</strong> <a href="x.com">${aluno.twitter}</a></p>
+        <p><strong>Instagram:</strong> <a href="instagram.com">${aluno.instagram}</a></p>
     `
 
     const info = `
         <img src="${aluno.avatar}" alt="avatarAluno" width="100px">
-        <p><strong>Aluno:</strong> ${aluno.nome}</p>
-        <p><strong>Curso:</strong> ${aluno.curso}</p>
-        <p><strong>Turma:</strong> ${aluno.turma}</p>
-    `
+        <div>
+            <p><strong>Aluno:</strong> ${aluno.nome}</p>
+            <p><strong>Curso:</strong> ${aluno.curso}</p>
+            <p><strong>Turma:</strong> ${aluno.turma}</p>
+        </div>
+        `
 
     miniBio.innerHTML = aluno.biografia;
     infoAluno.innerHTML = info;
@@ -167,7 +167,104 @@ function limitarTexto(texto, limite) {
     return texto.substring(0,limite) + '...';
 }
 
-botaoPesquisar.addEventListener('click', (e) => {
+
+document.addEventListener('DOMContentLoaded', function () {
+    var id = new URLSearchParams(location.search).get('id');
+    
+    fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=82e3160b362bd6e69585ed82fdf77260&language=pt-BR`)
+    .then(response => response.json())
+    .then(filme => {
+        const generos = filme.genres.map(g => g.name).join(', ');
+        const dataFormatada = formatarDataBR(filme.first_air_date);
+
+        document.getElementById('visaoGeral').innerHTML = `
+        <div class="col">
+            <div class="card">
+                <img src="https://image.tmdb.org/t/p/w1280${filme.backdrop_path}" alt="${filme.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${filme.name}</h5>
+                    <p class="card-text">${limitarTexto(filme.overview, 150) || 'Sem descrição disponível.'}</p>
+                    <p>${dataFormatada}</p>
+                    <p>${generos}</p>
+                    <button onclick="favoritarSerie(${id})" data-id="${filme.id} type="button" class="btn btn-primary">Favoritar</button>
+                </div>
+            </div>
+        </div>
+        `;
+    })
+    .catch(error => {
+        console.error('Erro ao buscar série:', error);
+        document.getElementById('visaoGeral').innerHTML = '<p class="text-danger">Erro ao carregar os dados.</p>';
+    });
+});
+
+function formatarDataBR(dataISO) {
+  if (!dataISO) return 'Data não disponível';
+
+  const partes = dataISO.split('-');
+  if (partes.length !== 3) return dataISO; // caso a data venha errada
+  return `${partes[2]}/${partes[1]}/${partes[0]}`;
+}
+
+function favoritarSerie(tmdbId){
+    const id = parseInt(tmdbId);
+    fetch(`http://localhost:3000/preferidas?tmdbId=${id}`)
+    .then(response => response.json())
+    .then(data => {
+        const jaFavoritado = data.some(item => parseInt(item.tmdbId) === id);
+        if(jaFavoritado){
+            alert('Este item já está nos seus favoritos!');
+    }else{
+        fetch('http://localhost:3000/preferidas', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({tmdbId: id})
+        })
+        .then(response => response.json())
+        .then(() => alert('Adicionado aos favoritos!'))
+        .catch(error => console.error('Erro ao adicionar favorito:', error));
+        }
+    })
+    .catch(error => console.error('Erro ao verificar favoritos:', error));
+}
+   
+function imprimeElenco(atores){
+     if (!atores || atores.length === 0) {
+        elenco.innerHTML = `<p class="text-danger">Elenco não encontrado.</p>`;
+        return;
+    }
+    
+    var card = '';
+
+    atores.forEach((atores) => {
+        card += `
+            <div class="col">
+            <div class="card">
+              <img src="https://image.tmdb.org/t/p/w185${atores.profile_path}" class="card-img-top" alt="${atores.name}">
+              <div class="card-body">
+                <h5 class="card-title">${atores.name}</h5>
+                <p class="card-text"><strong>Personagem: </strong>${atores.character}</p>
+              </div>
+            </div>
+          </div>
+        `;
+    })
+    document.getElementById('elenco').innerHTML = card;
+}
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get('id');
+
+fetch(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=82e3160b362bd6e69585ed82fdf77260&language=pt-BR`)
+    .then(response => response.json())
+    .then(data => {
+        imprimeElenco(data.cast);
+    })
+    .catch(error => console.error('Erro ao carregar elenco:', error));
+
+    botaoPesquisar.addEventListener('click', (e) => {
     e.preventDefault();
     const termo = inputPesquisa.value.trim();
     if (termo !== '') {
